0aa72f0495d9221b26703a7a444ec174980c03a7
sailyw <1152827086@qq.com>
2020-06-12 09:53:45 +0800

    实现路由嵌套跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Home from '@/components/Home.vue'
+import Cart from '@/views/Cart.vue'
+
+//Admin Components
+import Index from '@/views/admin/Index';
+import New from '@/views/admin/New';
+import Products from '@/views/admin/Products';
+import Edit from '@/views/admin/Edit';
 
 Vue.use(VueRouter)
 
@@ -11,13 +19,32 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/admin',
+    name: 'Admin',
+    component: Index,
+    children:[
+      {
+        path: 'new',
+        name: 'New',
+        component: New,
+      },
+      {
+        path: '',
+        name: 'Products',
+        component: Products,
+      },
+      {
+        path: 'edit/:id',
+        name: 'Edit',
+        component: Edit
+      },
+    ]
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
 ]
 
 const router = new VueRouter({
